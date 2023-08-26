abddbc0e5cff254cc5aa9a140381b1110f18fc6d
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2023-08-26 15:51:50 +0300

    2308261550

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Form from './form'
 export default function Page() {
     return (
         <div>
-            here be app
             <Form />
         </div>
     );
---
+++ app/wikiPage.js
@@ -0,0 +1,5 @@
+export default function WikiPage() {
+    return (<div>
+        wiki page
+    </div>)
+}
