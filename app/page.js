a8b7a9f8f3930f982e56d5eb1249996a4feefc81
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2023-08-29 11:37:37 +0300

    fixed hydration warning by switching pages list to client

--- app/page.js
+++ app/page.js
@@ -1,17 +1,20 @@
 import { Suspense } from 'react';
 import Form from './form'
 import WikiPage from './wikiPage';
+import WikiPagesList from './wikiPagesList';
 
 const api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
 let query = "test";
 
+let pagesListToRender = [
+  <WikiPage page={{title: "test title 1"}}></WikiPage>,
+  <WikiPage page={{title: "test title 2"}}></WikiPage>,
+  <WikiPage page={{title: "test title 3"}}></WikiPage>
+]
+
 async function getData() {
     const res = await fetch(`${api + query}`)
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-   
     if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
       throw new Error('Failed to fetch data')
     }
    
@@ -19,14 +22,13 @@ async function getData() {
   }
 
 async function buildPagesList(data) {
-    //const data = await getData();
     let pagesList = [];
     const pagesData = data.query.pages;
     console.log('buildPagesList: ' + pagesData[14892].title);
     for(const page in pagesData) {
         //console.log('pageData: ' + pageData);
         console.log('title: ' + pagesData[page].title);
-        pagesList.push(<WikiPage page={pagesData[page]}></WikiPage>);
+        pagesList.push(<WikiPage page={{title: pagesData[page].title}}></WikiPage>);
     }
     console.log('pagesList: ' + JSON.stringify(pagesList[0]));
     return pagesList;
@@ -40,7 +42,11 @@ export default async function Page() {
     return (
         <div>
             <Form />
-            {pagesListToRender}
+            <WikiPagesList pages={pagesListToRender}></WikiPagesList>
         </div>
     );
+  }
+
+  export const metadata = {
+    title: 'nextjs-wikipedia-search',
   }
---
+++ app/wikiPagesList.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default async function WikiPagesList(props) {
+    return(
+        <>
+            {props.pages}
+        </>
+    )
+}
